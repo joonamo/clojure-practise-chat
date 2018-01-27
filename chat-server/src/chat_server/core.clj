bf03d2d1378be0c2ebdd32615e227d2cc65d611e
joonamo <joonamo@users.noreply.github.com>
2018-01-27 23:14:46 +0200

    Notify channel on user renaming.

--- chat-server/src/chat_server/core.clj
+++ chat-server/src/chat_server/core.clj
@@ -145,16 +145,28 @@
       {:user (get-name-and-id-for-user conn) :channel target-channel :message message})
     (reply-error conn "send-message action payload didn't have message and/or target-channel!"))))
 
-; (defn notify-channel-user-rename
-;   [channel old-name new-name])
+(defn notify-channel-user-rename
+  [channel user-id old-name new-name]
+  (if (nil? channel)
+    nil
+    (send-to-channel channel "user-rename" 
+      {:user {:name old-name :id user-id} :new-name new-name :old-name old-name})))
 
-; (defn notify-user-rename
-;   [channels old-name new-name])
+(defn notify-user-rename
+  [channels user-id old-name new-name]
+  (notify-channel-user-rename (first channels) user-id old-name new-name)
+  (if (empty? channels)
+    nil
+    (recur (rest channels) user-id old-name new-name)))
 
 (defn handle-action-change-name
   [payload conn]
   (if (contains? payload :new-name)
-    (rename-user! conn (payload :new-name)) 
+    (let [user (get-name-and-id-for-user conn)
+          new-name (payload :new-name)
+          channels ((@connections-to-users conn) :channels)]
+        (rename-user! conn new-name)
+        (notify-user-rename channels (user :id) (user :name) new-name)) 
     (reply-error conn "change-name action payload didn't include new-name!")))
 
 (defn handle-action-join-channel
